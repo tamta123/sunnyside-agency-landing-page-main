7c050be68c18df43943a68b25121f7e92493c941
tamta123 <tamtag186@gmail.com>
2023-04-08 15:20:58 +0400

    burger menu

---
+++ script.js
@@ -0,0 +1,8 @@
+const nav = document.getElementById("myLinks");
+function myFunction() {
+    if (nav.style.display === "block") {
+      nav.style.display = "none";
+    } else {
+      nav.style.display = "block";
+    }
+  }
