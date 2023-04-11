3ef84b9a8343982596ed1e647a35454cd79c67c3
tamta123 <tamtag186@gmail.com>
2023-04-11 12:38:55 +0400

    added triangle on mobile menu & hover on icons

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 const nav = document.getElementById("myLinks");
+const triangle = document.querySelector('.triangle')
 function myFunction() {
     if (nav.style.display === "block") {
       nav.style.display = "none";
+      triangle.style.display = 'none'
     } else {
       nav.style.display = "block";
+      triangle.style.display = 'block'
     }
   }
